6dcaca82f28b7f09c5142b210fa404d21782eea9
Carlo Gilmar <carlogilmar12@gmail.com>
2020-02-05 13:14:54 -0600

    rueba de Concepto sobre Riak

---
+++ lib/civile.ex
@@ -0,0 +1,18 @@
+defmodule Civile do
+  @moduledoc """
+  Documentation for Civile.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Civile.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/civile_test.exs
@@ -0,0 +1,8 @@
+defmodule CivileTest do
+  use ExUnit.Case
+  doctest Civile
+
+  test "greets the world" do
+    assert Civile.hello() == :world
+  end
+end
